2668adf867379f3e9a47507f9fc79425ec987adb
lucassfontes <80824656+lucassfontes@users.noreply.github.com>
2021-08-08 17:31:00 +0200

    Add files via upload

---
+++ script_inicio.js
@@ -0,0 +1,15 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+
+let logado = document.querySelector('#logado')
+
+logado.innerHTML = `Olá ${userLogado.nome} <br>Você está logado!`
+
+if(localStorage.getItem('token') == null){
+    window.location.href='index.html'
+}
+
+function sair(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('userLogado')
+    window.location.href='index.html'
+}
