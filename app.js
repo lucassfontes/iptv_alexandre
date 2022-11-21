6f84cdabdfa4a45fa1528f6523ff512704d72151
lucassfontes <80824656+lucassfontes@users.noreply.github.com>
2022-11-21 14:00:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+
+//const conn_db = require('./models/db')
+
+//app.use(express.static(__dirname + '/SERIADOS'))
+
+app.get("/alisson", async(req, res) => {
+    //res.sendFile(__dirname + '/alisson-full.m3u')
+    res.redirect('http://acsa.ws:80/get.php?username=lucassfontessantos&password=Br452668')
+})
+
+
+app.listen(3001, () => {
+    console.log("Servidor iniciado na porta 3001: http://localhost:3001")
+    console.log("Para derrubar o servidor digite: ctrl + c")
+})
+
+//Para roda o servidor com o nodemon
+// user o comando nodemon app.js
