ce5cd0a17330e185f9100a74b6789e3cfd4bf9ed
lucassfontes <80824656+lucassfontes@users.noreply.github.com>
2024-01-26 16:00:14 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-let modal = document.getElementById('modal')
-let play = document.getElementById('play')
-let close = document.getElementById('close')
-let videoPlay = document.getElementById('videoPlay')
-
-play.addEventListener('click', ()=>{
-
-    
-    modal.setAttribute('style','display:block')
-    videoPlay.play()
-})
-
-close.addEventListener('click', ()=>{
-    videoPlay.pause()
-    modal.setAttribute('style','display:none')
-
-})
 
 
